0c693fbaad271bc5a5412394849bd2404434d7d8
BranRead <branrtread@gmail.com>
2024-01-22 06:48:11 -0400

    Buttons all work like they should
    Need border around buttons and a randomize function

--- Assignment2/face_maker/MainWindow.xaml.cs
+++ Assignment2/face_maker/MainWindow.xaml.cs
@@ -20,6 +20,15 @@ namespace face_maker
     /// </summary>
     public partial class MainWindow : Window
     {
+        string[] hairArray = { "hair1.png", "hair2.png", "hair3.png", "hair4.png", "hair5.png", "hair6.png", "hair7.png", "hair8.png" };
+        string[] eyesArray = { "eyes1.png", "eyes2.png", "eyes3.png", "eyes4.png", "eyes5.png", "eyes6.png", "eyes7.png", "eyes8.png" };
+        string[] noseArray = { "nose1.png", "nose2.png", "nose3.png", "nose4.png", "nose5.png", "nose6.png", "nose7.png" };
+        string[] mouthArray = { "mouth1.png", "mouth2.png", "mouth3.png", "mouth4.png", "mouth5.png", "mouth6.png", "mouth7.png" };
+        int hairArrayIndex = 0;
+        int eyesArrayIndex = 0;
+        int noseArrayIndex = 0;
+        int mouthArrayIndex = 0;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -27,42 +36,71 @@ namespace face_maker
 
         private void Hair_Button_Back(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Hair back");
+            Previous_Image(Hair, hairArray, ref hairArrayIndex);
         }
 
         private void Hair_Button_Forward(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Hair forward");
+            Next_Image(Hair, hairArray, ref hairArrayIndex);
         }
 
         private void Eyes_Button_Back(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Eyes back");
+            Previous_Image(Eyes, eyesArray, ref eyesArrayIndex);
         }
 
         private void Eyes_Button_Forward(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Eyes forward");
+            Next_Image(Eyes, eyesArray, ref eyesArrayIndex);
+
         }
 
         private void Nose_Button_Back(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Nose back");
+            Previous_Image(Nose, noseArray, ref noseArrayIndex);
+
         }
 
         private void Nose_Button_Forward(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Nose forward");
+            Next_Image(Nose, noseArray, ref noseArrayIndex);
+
         }
 
         private void Mouth_Button_Back(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Mouth back");
+            Previous_Image(Mouth, mouthArray, ref mouthArrayIndex);
         }
 
         private void Mouth_Button_Forward(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Mouth forward");
+            Next_Image(Mouth, mouthArray, ref mouthArrayIndex);
+
+        }
+
+        private static void Previous_Image(Image image, string[] array, ref int index)
+        {
+            index--;
+            if (index < 0)
+            {
+                index = array.Length - 1;
+            }
+
+            string source = "C:\\Users\\Brandon\\Desktop\\ITProgramming\\Semester 4\\PROG2500\\Assignments\\Assignment2\\face_maker\\images\\" + array[index];
+            image.Source = new BitmapImage(new Uri(source, UriKind.Absolute));
+        }
+
+        private static void Next_Image(Image image, string[] array, ref int index)
+        {
+            index++;
+            if (index > array.Length -1)
+            {
+                index = 0;
+            }
+
+            string source = "C:\\Users\\Brandon\\Desktop\\ITProgramming\\Semester 4\\PROG2500\\Assignments\\Assignment2\\face_maker\\images\\" + array[index];
+            image.Source = new BitmapImage(new Uri(source, UriKind.Absolute));
         }
     }
+
 }
