cf10e9036e6ffabd7e4735f26108d7b8499303db
BranRead <branrtread@gmail.com>
2024-03-06 11:26:12 -0400

    Finished assignment, also changed unit tests for 2D grouping

--- Assignment8/M08_Grouping_Unit_Testing/M08_Grouping_ProjTests1/GroupingTests.cs
+++ Assignment8/M08_Grouping_Unit_Testing/M08_Grouping_ProjTests1/GroupingTests.cs
@@ -13,7 +13,7 @@ namespace M01_First_WPF_Proj.Tests
     public class GroupingTests
     {
         [TestMethod()]
-        public void GetGroupTest()
+        public void GetGroupTest1()
         {
             int distanceThreshold = 100;
             int num_of_points = 2;
@@ -27,13 +27,74 @@ namespace M01_First_WPF_Proj.Tests
             int Actual_Group_Count = groupings.Count;
             int Expected_Group_Count = 2;
             Assert.AreEqual(Actual_Group_Count, Expected_Group_Count);
+        }
 
-            //Comparing points 
-            Point Actual_Point = groupings[0][0];
-            Point Expected_Point = new Point(0, 0);
-            Boolean Is_Same_Point = (Actual_Point.X == Expected_Point.X &&
-                Actual_Point.Y == Expected_Point.Y);
-            Assert.IsTrue(Is_Same_Point);
+        [TestMethod()]
+        public void GetGroupTest2()
+        {
+            int distanceThreshold = 100;
+            int num_of_points = 2;
+            Grouping g = null;
+            g = new Grouping(distanceThreshold, num_of_points);
+            g.MasterList[0] = new Point(0, 0);
+            g.MasterList[1] = new Point(0, 100);
+
+            List<List<Point>> groupings = g.applyThreseholdsMakeGroups();
+
+            int Actual_Group_Count = groupings.Count;
+            int Expected_Group_Count = 2;
+            Assert.AreEqual(Actual_Group_Count, Expected_Group_Count);
+        }
+
+        [TestMethod()]
+        public void GetGroupTest3()
+        {
+            int distanceThreshold = 100;
+            int num_of_points = 2;
+            Grouping g = null;
+            g = new Grouping(distanceThreshold, num_of_points);
+            g.MasterList[0] = new Point(0, 0);
+            g.MasterList[1] = new Point(86, 50);
+
+            List<List<Point>> groupings = g.applyThreseholdsMakeGroups();
+
+            int Actual_Group_Count = groupings.Count;
+            int Expected_Group_Count = 1;
+            Assert.AreEqual(Actual_Group_Count, Expected_Group_Count);
+        }
+
+        [TestMethod()]
+        public void GetGroupTest4()
+        {
+            int distanceThreshold = 100;
+            int num_of_points = 2;
+            Grouping g = null;
+            g = new Grouping(distanceThreshold, num_of_points);
+            g.MasterList[0] = new Point(87, 50);
+            g.MasterList[1] = new Point(0, 0);
+
+            List<List<Point>> groupings = g.applyThreseholdsMakeGroups();
+
+            int Actual_Group_Count = groupings.Count;
+            int Expected_Group_Count = 2;
+            Assert.AreEqual(Actual_Group_Count, Expected_Group_Count);
+        }
+
+        [TestMethod()]
+        public void GetGroupTest5()
+        {
+            int distanceThreshold = 100;
+            int num_of_points = 2;
+            Grouping g = null;
+            g = new Grouping(distanceThreshold, num_of_points);
+            g.MasterList[0] = new Point(50, 35);
+            g.MasterList[1] = new Point(136, 85);
+
+            List<List<Point>> groupings = g.applyThreseholdsMakeGroups();
+
+            int Actual_Group_Count = groupings.Count;
+            int Expected_Group_Count = 1;
+            Assert.AreEqual(Actual_Group_Count, Expected_Group_Count);
         }
     }
 }
